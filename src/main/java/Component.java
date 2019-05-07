d2b351b3c555a7487cd9af8954e8d5671269af10
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-05-07 18:53:18 -0400

    Component interface and class renamed

--- src/main/java/Component.java
+++ src/main/java/Component.java
@@ -1,5 +1,5 @@
 package main.java;
 
-public class OnOffComponent {
+public class Component {
    
 }
--- src/main/java/IComponent.java
+++ src/main/java/IComponent.java
@@ -1,5 +1,5 @@
 package main.java;
 
-public class IOnOffComponent {
+public class IComponent {
 
 }
